ab6cafc416733cb7c05cdc5a4917f2f04464ae87
lubnaz117 <lubnaz117@gmail.com>
2020-03-22 15:58:36 -0700

    Convert spaceship pos to int right before draw

--- Spaceship.py
+++ Spaceship.py
@@ -14,15 +14,14 @@ class Spaceship():
         self._color = 0, 255, 0
 
     def draw(self, screen):
+        self._pos = [int(self.state[0]), int(self.state[1])]
+
         pygame.draw.circle(screen, self._color, self._pos, self._radius)
 
     def update(self, dt, action):
-        #self.color = GREEN
         
         fun = gravity2D
         t = 0
         self.state = propagate(fun, t, self.state, dt, action)
-
-        self._pos = [int(self.state[0]), int(self.state[1])]
         
         #print(self.state)
