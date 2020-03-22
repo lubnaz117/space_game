af707f8aee7246ac57253b6714de671cf7362a5a
Ian <ian.elliott@colorado.edu>
2020-03-22 13:58:17 -0400

    More whitespace

--- space_game.py
+++ space_game.py
@@ -33,43 +33,43 @@ def update(dt):
         # Handle other events as you wish.
  
 def draw(screen, ship):
-  """
-  Draw things to the window. Called once per frame.
-  """
-  screen.fill((0, 0, 0)) # Fill the screen with black.
-  ship.draw(screen)
-  
-  # Redraw screen here.
-  
-  # Flip the display so that the things we drew actually show up.
-  pygame.display.flip()
+    """
+    Draw things to the window. Called once per frame.
+    """
+    screen.fill((0, 0, 0)) # Fill the screen with black.
+    ship.draw(screen)
+
+    # Redraw screen here.
+
+    # Flip the display so that the things we drew actually show up.
+    pygame.display.flip()
  
 def runPyGame():
-  # Initialise PyGame.
-  pygame.init()
+    # Initialise PyGame.
+    pygame.init()
 
-  # Initialize Spaceship
-  ship = Spaceship()
-  
-  # Set up the clock. This will tick every frame and thus maintain a relatively constant framerate. Hopefully.
-  fps = 60.0
-  fpsClock = pygame.time.Clock()
-  
-  # Set up the window.
-  width, height = 640, 480
-  screen = pygame.display.set_mode((width, height))
-  
-  # screen is the surface representing the window.
-  # PyGame surfaces can be thought of as screen sections that you can draw onto.
-  # You can also draw surfaces onto other surfaces, rotate surfaces, and transform surfaces.
-  
-  # Main game loop.
-  dt = 1/fps # dt is the time since last frame.
-  while True: # Loop forever!
-    update(dt) # You can update/draw here, I've just moved the code for neatness.
-    draw(screen, ship)
-    
-    dt = fpsClock.tick(fps)
+    # Initialize Spaceship
+    ship = Spaceship()
+
+    # Set up the clock. This will tick every frame and thus maintain a relatively constant framerate. Hopefully.
+    fps = 60.0
+    fpsClock = pygame.time.Clock()
+
+    # Set up the window.
+    width, height = 640, 480
+    screen = pygame.display.set_mode((width, height))
+
+    # screen is the surface representing the window.
+    # PyGame surfaces can be thought of as screen sections that you can draw onto.
+    # You can also draw surfaces onto other surfaces, rotate surfaces, and transform surfaces.
+
+    # Main game loop.
+    dt = 1/fps # dt is the time since last frame.
+    while True: # Loop forever!
+        update(dt) # You can update/draw here, I've just moved the code for neatness.
+        draw(screen, ship)
+
+        dt = fpsClock.tick(fps)
 
 if __name__ == "__main__":
     runPyGame()
